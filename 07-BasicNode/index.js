8e995a9e7411a4e99d365b5dfc08dd5a179fbf3f
Anton-14-7 <98771536+Anton-14-7@users.noreply.github.com>
2023-10-24 23:32:46 -0600

    Add files via upload

---
+++ 07-BasicNode/index.js
@@ -0,0 +1,19 @@
+
+const superheroes = require('superheroes');
+ 
+superheroes.all;
+
+ 
+superheroes.random();
+
+
+
+const supervillains = require('supervillains');
+
+supervillains.all;
+
+
+supervillains.random();
+
+
+console.log(supervillains.random() + " has aterrorized the city but dont worry: " + superheroes.random() + " is here to help" );
